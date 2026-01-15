6fc475ec03e1ad65d519dd51c0675bc8daf663cd
Alistair Windsor <awindsor@memphis.edu>
2026-01-15 20:07:27 +0000

    Debug changes

--- uofm_calendar_import.py
+++ uofm_calendar_import.py
@@ -305,14 +305,12 @@ def parse_deadlines_drop_withdraw(dates_html: str) -> Tuple[Optional[date], Opti
         if re.search(r"Drop Period", l, re.IGNORECASE):
             in_drop = True
             in_withdraw = False
-            print(l)
             continue
         if re.search(r"Withdrawal Period", l, re.IGNORECASE):
             in_drop = False
             in_withdraw = True
-            print(l)
             continue
-
+    print(l)
         # Grab the first FULL line in each section (ignore WIN, 1ST, 2ND, TN eCampus, etc.)
         if in_drop and full_drop_line is None and re.match(r"^\s*FULL\b", l):
             full_drop_line = l
